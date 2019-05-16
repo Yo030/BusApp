ef80fc83c4e0aa2b5c133514c18059d44fd1d998
Yo030 <seryrak49@gmail.com>
2019-05-16 14:20:15 -0500

    Se agrego la base.

---
+++ Assets/Scripts/ChooseRoute.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseRoute : MonoBehaviour
+{
+    public Dropdown routeContainer;
+
+    public List<GameObject> route = new List<GameObject>();
+
+    public void SetRoute()
+    {
+        switch(routeContainer.value)
+        {
+            case 1:
+                route[0].SetActive(true);
+                route[1].SetActive(false);
+                break;
+            case 2:
+                route[0].SetActive(false);
+                route[1].SetActive(true);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/FollowRoute.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRoute : MonoBehaviour
+{
+    //[SerializeField]
+
+    public Transform[] marks;
+    public float speed;
+    Vector2 target;
+    int index = 1;
+    string direction = "front";
+    int size;
+
+    private void Start()
+    {
+        size = marks.Length;
+    }
+    void Update()
+    {
+        target = marks[index].transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, target, speed);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (direction == "front")
+        {
+            index += 1;
+        } else if (direction == "back")
+        {
+            index -= 1;
+        }
+
+        if (index == size)
+        {
+            direction = "back";
+            index -= 2;
+        }
+        if (index == 0)
+        {
+            direction = "front";
+            index += 2;
+        }
+    }
+}

afb3d8f89bc165dd36ecc353dd800d46831993d4
Yo030 <seryrak49@gmail.com>
2019-05-16 16:04:52 -0500

    Se Agregaron cambios a la escena de cargo de tarjeta.

---
+++ Assets/Scripts/OcultarObjetos.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcultarObjetos : MonoBehaviour
+{
+    public GameObject[] Ocultar;
+    public GameObject[] Aparecer;
+
+    public void OcultarLista()
+    {
+        for (int i = 0; i  < Ocultar.Length; i++)
+        {
+            Ocultar[i].SetActive(false);
+        }
+    }
+
+    public void AparecerLista()
+    {
+        for (int i = 0; i < Aparecer.Length; i++)
+        {
+            Aparecer[i].SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/Scripts/RevisarInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RevisarInput : MonoBehaviour
+{
+    [SerializeField]
+    private InputField Monto;
+
+    public Text Confirmacion;
+
+    void LimiteDeRecarga(float Tam, InputField Calificacion)
+    {
+        if (Tam >= 11)
+        {
+            Calificacion.text = "300";
+        }
+    }
+
+    void Update()
+    {
+        Confirmacion.text = "Estas segurao que quieres ingresar :" + Monto + "?";
+    }
+
+}

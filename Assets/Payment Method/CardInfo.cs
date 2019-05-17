b7e0e4b9a7dad8e1a4b75e4def570db270079fc2
FernanD02 <43126842+FernanD02@users.noreply.github.com>
2019-05-17 10:51:22 -0500

    Pantalla para agregar tarjeta

---
+++ Assets/Payment Method/CardInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CardInfo : MonoBehaviour
+{
+    private string name;
+    private string Month;
+    private string Year;
+    private string Number;
+    private string security;
+
+    public CardInfo(string _name, string _month, string _year, string _Number, string _security)
+    {
+        name = _name;
+        Month = _month;
+        Year = _year;
+        Number = _Number;
+        security = _security;
+    }
+}
---
+++ Assets/Payment Method/SaveInfo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveInfo : MonoBehaviour
+{
+    private string name;
+    private string date;
+    private string number;
+    private string security;
+
+    private int[] Cards;
+    private double[] numberCard = new double[16];
+    private int validCard;
+
+    public InputField NameField;
+    public InputField MonthField;
+    public InputField YearField;
+    public InputField NumberField;
+    public InputField SecurityField;
+
+    public void saveInfo()
+    {
+        if (NumberField.text.Length == 16)
+        {
+            for (int i = 0; i < NumberField.text.Length; i++)
+            {
+                numberCard[i] = char.GetNumericValue(NumberField.text[i]);
+                if (i % 2 == 0)
+                {
+                    //Debug.Log(numberCard[i]);
+                    switch (numberCard[i])
+                    {
+                        case 5:
+                            numberCard[i] = 1;
+                            break;
+                        case 6:
+                            numberCard[i] = 3;
+                            break;
+                        case 7:
+                            numberCard[i] = 5;
+                            break;
+                        case 8:
+                            numberCard[i] = 7;
+                            break;
+                        case 9:
+                            numberCard[i] = 9;
+                            break;
+                        default:
+                            numberCard[i] += numberCard[i];
+                            break;
+                    }
+                    
+                }
+                Debug.Log(numberCard[i]);
+                validCard += Convert.ToInt32(numberCard[i]);
+                
+            }
+            Debug.Log("Valid Card: " + validCard);
+
+        }
+        else
+        {
+            Debug.Log("numero no valido");
+        }
+        //for (int i = 0; i < cardNumbers.Length; i++)
+        //{
+
+        //}
+
+        ////CardInfo card = new CardInfo(NameField.text, DateField.text, NumberField.text, SecurityField.text);
+        //BinaryFormatter bf = new BinaryFormatter();
+        //FileStream file = File.Create(Application.persistentDataPath + "CardInfo")
+    }
+}

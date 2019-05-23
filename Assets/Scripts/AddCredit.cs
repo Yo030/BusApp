eae2904b3aad4b6118ff0d72261227c6c2cd3b2e
yosoykyukim <yosoykyukim@gmail.com>
2019-05-23 02:03:17 -0500

    this

---
+++ Assets/Scripts/AddCredit.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddCredit : MonoBehaviour
+{
+    public InputField field;
+    private string input;
+
+    void addCredit(int _addedBalance)
+    {
+        switch (numeroTarjeta.tarjeta)
+        {
+            case 1:
+                numeroTarjeta.saldo_1 += _addedBalance;
+                break;
+            case 2:
+                numeroTarjeta.saldo_2 += _addedBalance;
+                break;
+            case 3:
+                numeroTarjeta.saldo_3 += _addedBalance;
+                break;
+        }
+    }
+
+    public void setBalance()
+    {
+        input = field.text;
+        int inputNum = 0;
+        int.TryParse(input, out inputNum);
+
+        addCredit(inputNum);
+        Debug.Log(inputNum+1);
+    }
+
+    public void debugBalance()
+    {
+        Debug.Log(numeroTarjeta.saldo_1);
+    }
+
+}
+
+
+//
